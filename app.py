9a9d157cf6ead5f2193c653cc1ba931a56aa7e24
Mirza Hashir Baig <imhashir@users.noreply.github.com>
2022-12-15 00:07:12 +0500

    Added gunicorn

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, request
 from chat_reply import ChatReply
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 
-@application.route("/get_reply_suggestions", methods=["GET"])
+@app.route("/get_reply_suggestions", methods=["GET"])
 def get_reply_suggestions():
     messages = request.get_json()
     suggestions = ChatReply.get_reply_suggestions(messages)
     return suggestions
 
 
-@application.route("/", methods=['GET'])
+@app.route("/", methods=['GET'])
 def index():
     return "Send a get request to the get_reply_suggestions endpoint"
 
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    app.run(debug=True)

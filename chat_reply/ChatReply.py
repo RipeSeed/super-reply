ce19462ffdc792099eae18053a768bb741cb9f88
RizwanAmjad <rizwanamjadms@gmail.com>
2022-12-29 13:09:31 +0500

    Added support for multiple tokens and added dynamic token selection based on tokens generated from the time server is up

--- chat_reply/ChatReply.py
+++ chat_reply/ChatReply.py
@@ -4,12 +4,31 @@ from .generate_message import for_change_tone, for_reply_suggestions
 from .utils import sanitize_output
 from .count_request_info import update_request_info
 
-OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
+OPEN_AI_API_KEY = [
+    {"key": key, "tokens_count": 0} for key in os.environ.get(
+        'OPEN_AI_API_KEY').split('::')
+]
 
-# load Open AI API key from environment variable
-openai.api_key = OPEN_AI_API_KEY
+key_index = 0
 
-completion = openai.Completion()
+
+def __choose_api_key():
+    global completion, key_index
+
+    # choose the API key to use
+    openai.api_key = OPEN_AI_API_KEY[key_index]['key']
+
+    token_counts = [item['tokens_count'] for item in OPEN_AI_API_KEY]
+    min_index = token_counts.index(min(token_counts))
+
+    completion = openai.Completion()
+
+    key_index = min_index
+    return min_index
+
+
+def __update_tokens(tokens: int, api_index):
+    OPEN_AI_API_KEY[api_index]['tokens_count'] += tokens
 
 
 def __count_words(messages: list):
@@ -19,6 +38,8 @@ def __count_words(messages: list):
 
 def get_reply_suggestions(email, messages: list, suggestion_count=3,
                           other_than=None, reply_tone=None, reply_from=None, reply_to=None, word_count=None):
+    api_index = __choose_api_key()
+
     message = for_reply_suggestions(
         messages, other_than, reply_tone, reply_from, reply_to, word_count)
 
@@ -26,6 +47,8 @@ def get_reply_suggestions(email, messages: list, suggestion_count=3,
         prompt=message, engine="text-davinci-003", stop=['\nHuman'], temperature=0.9,
         top_p=1, frequency_penalty=0.5, presence_penalty=0, best_of=suggestion_count,
         max_tokens=2*word_count if word_count != None else 2*(int(len(message.split(' '))/len(messages))), n=suggestion_count)
+
+    __update_tokens(response.usage.total_tokens, api_index)
 
     suggestions = list()
     for choice in response.choices:
@@ -39,6 +62,8 @@ def get_reply_suggestions(email, messages: list, suggestion_count=3,
 
 def change_tone(email, messages: list, suggestion_count=3,
                 other_than=None, reply_tone=None, reply_from=None, reply_to=None, word_count=None):
+    api_index = __choose_api_key()
+
     message = for_change_tone(messages, other_than,
                               reply_tone, reply_from, reply_to, word_count)
 
@@ -46,6 +71,8 @@ def change_tone(email, messages: list, suggestion_count=3,
         prompt=message, engine="text-davinci-003", stop=['\nHuman'], temperature=0.9,
         top_p=1, frequency_penalty=0.5, presence_penalty=0, best_of=suggestion_count,
         max_tokens=2*word_count if word_count != None else 2*(int(len(message.split(' '))/len(messages))), n=suggestion_count)
+
+    __update_tokens(response.usage.total_tokens, api_index)
 
     suggestions = list()
     for choice in response.choices:

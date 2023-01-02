a85ddc7c9477349705eeca82406607ffffa946fa
RizwanAmjad <rizwanamjadms@gmail.com>
2023-01-02 14:05:12 +0500

    moved free user and payed user word limit to environment variables

--- middlewares/limits_const.py
+++ middlewares/limits_const.py
@@ -11,6 +11,9 @@ change_tone_limits = {
 }
 
 word_limits = {
-    "FREE_USER_WORD_LIMIT": 10000,
-    "PAYED_USER_WORD_LIMIT": math.inf
+    "FREE_USER_WORD_LIMIT": int(os.environ.get('FREE_USER_WORD_LIMIT', 10000)),
+    "PAYED_USER_WORD_LIMIT": int(os.environ.get('PAYED_USER_WORD_LIMIT', -1))
 }
+
+if word_limits['PAYED_USER_WORD_LIMIT'] < 0:
+    word_limits['PAYED_USER_WORD_LIMIT'] = math.inf

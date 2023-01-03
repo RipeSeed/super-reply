9b96be4af1e56f50580004deb94c5ea48281442a
RizwanAmjad <rizwanamjadms@gmail.com>
2023-01-03 12:50:20 +0500

    Refactored the getting limits from environment variables logic

--- middlewares/limits_const.py
+++ middlewares/limits_const.py
@@ -1,9 +1,17 @@
 import math
 import os
 
+
+def __parse_limit(limit):
+    try:
+        return int(limit)
+    except:
+        return math.inf
+
+
 suggestions_limits = {
-    "FREE_USER_LIMIT_DAILY": int(os.environ.get('SUGESSION_DAILY_LIMIT', 5)),
-    "FREE_USER_LIMIT_MONTHLY": int(os.environ.get('SUGESSION_MONTHLY_LIMIT', 75))
+    "FREE_USER_LIMIT_DAILY": __parse_limit(os.environ.get('SUGESSION_DAILY_LIMIT', 5)),
+    "FREE_USER_LIMIT_MONTHLY": __parse_limit(os.environ.get('SUGESSION_MONTHLY_LIMIT', 75))
 }
 
 change_tone_limits = {
@@ -11,9 +19,6 @@ change_tone_limits = {
 }
 
 word_limits = {
-    "FREE_USER_WORD_LIMIT": int(os.environ.get('FREE_USER_WORD_LIMIT', 10000)),
-    "PAYED_USER_WORD_LIMIT": int(os.environ.get('PAYED_USER_WORD_LIMIT', -1))
+    "FREE_USER_WORD_LIMIT": __parse_limit(os.environ.get('FREE_USER_WORD_LIMIT', 10000)),
+    "PAYED_USER_WORD_LIMIT": __parse_limit(os.environ.get('PAYED_USER_WORD_LIMIT'))
 }
-
-if word_limits['PAYED_USER_WORD_LIMIT'] < 0:
-    word_limits['PAYED_USER_WORD_LIMIT'] = math.inf
